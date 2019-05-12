93a86f81d0f6046896ee1f7b3f241944741e90a3
Khaliq Gant <khaliqgant@gmail.com>
2019-05-12 18:56:30 +0200

    start work on search functionality

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,10 +1,82 @@
 import * as React from 'react';
 
-class Search extends React.Component<{}, {}> {
+interface SearchProps {
+}
 
-    render() {
-        return 'Search Bar';
+interface SearchState {
+  active: boolean;
+}
+
+class Search extends React.Component<SearchProps, SearchState> {
+
+  private searchInput: any;
+  private setTextInputRef: any;
+
+  constructor(props: SearchProps) {
+    super(props);
+
+    this.searchInput = null;
+
+    this.setTextInputRef = ((el: any) => {
+      this.searchInput = el;
+    });
+
+    this.onDocumentKeyDown = this.onDocumentKeyDown.bind(this);
+
+    this.state = {
+      active: false
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.onDocumentKeyDown);
+  }
+
+  onFocus(e: any) {
+    this.setState({
+      active: true
+    });
+  }
+
+  onDocumentKeyDown(e: any) {
+    switch (e.keyCode) {
+      case 191:
+        this.focusSearch();
     }
+  }
+
+  onKeyDown(e: any) {
+    switch (e.keyCode) {
+      case 13:
+        this.submitSearch();
+    }
+  }
+
+  focusSearch() {
+    this.searchInput.focus();
+    console.log(this.searchInput)
+    // if doing this, the slash goes into the text input
+  }
+
+  submitSearch() {
+    console.log('submit')
+  }
+
+  render() {
+    return (
+      <span>
+      <input
+        type="text"
+        name="search"
+        placeholder="Search"
+        ref={this.setTextInputRef}
+        onFocus={(e) => this.onFocus(e)}
+        onKeyDown={(e) => this.onKeyDown(e)}
+      />
+      <i className="fa fa-search"></i>
+      </span>
+    );
+  }
 
 }
 export default Search;
